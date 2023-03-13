9fcf40815a9154ee6d21f11f36bb193da712f2d0
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2023-03-13 17:01:49 +0600

    Initial Commit

---
+++ MVVVM+C+Registr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVVM+C+Registr
+//
+//  Created by zalkarbek on 13/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
